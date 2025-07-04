b3dff068d70db81f0aba9f5b4930142c4403154f
Anton Haba <lykoiiiko@gmail.com>
2025-07-04 09:28:01 +0300

    исправил замечания по homework-1

--- posts-app/src/entities/post/ui/PostCard.tsx
+++ posts-app/src/entities/post/ui/PostCard.tsx
@@ -1,20 +1,22 @@
-import type { FC } from "react";
+import type { FC } from "react"
 
 type Props = {
 	post: {
-		userId: number;
-		id: number;
-		title: string;
-		body: string;
-	};
-};
+		userId: number
+		id: number
+		title: string
+		body: string
+	}
+}
 
 const PostCard: FC<Props> = ({ post }) => {
 	return (
-		<div>
-			<h5>{post.title}</h5>
-			<p>{post.body}</p>
-		</div>
-	);
-};
-export default PostCard;
+		<li>
+			<div>
+				<h5>{post.title}</h5>
+				<p>{post.body}</p>
+			</div>
+		</li>
+	)
+}
+export default PostCard
